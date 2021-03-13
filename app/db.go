dfcb01edf75779887426bac1f99a0d97b8304e45
gtindo <gtyletindo@gmail.com>
2021-03-13 22:07:10 +0100

    add redis db

--- app/db.go
+++ app/db.go
@@ -1,11 +1,17 @@
 package app
 
 import (
+	"context"
 	"database/sql"
 	"log"
+
+	"github.com/go-redis/redis/v8"
 )
 
 var db *sql.DB
+
+var ctx = context.Background()
+var rdb *redis.Client
 
 func InitDb(dbStr string) {
 	db, err := sql.Open("mysql", dbStr)
@@ -18,4 +24,24 @@ func InitDb(dbStr string) {
 	}
 
 	log.Println("Connection established with MYSQL DB")
+}
+
+func InidRedisDb(addr string, password string, dbNum int) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       dbNum,
+	})
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = rdb.Get(ctx, "key").Result()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connection established with Redis Server")
 }
--- main.go
+++ main.go
@@ -17,9 +17,14 @@ func main() {
 	port := os.Getenv("PORT")
 	dbStr := os.Getenv("MYSQL_STRING")
 
+	redisAddr := os.Getenv("REDIS_ADDRESS")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+	redisDb := 0
+
 	app.InitDb(dbStr)
+	app.InidRedisDb(redisAddr, redisPassword, redisDb)
 
 	defer http.ListenAndServe(":"+port, app.GetAppRouter())
 
-	log.Println("Server listening on port ", port)
+	log.Println("Http Server listening on port", port)
 }
